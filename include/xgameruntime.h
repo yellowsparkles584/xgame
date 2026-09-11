1b51cde527aea01a3df4414f2d002579d2dc5ec1
yellowsparkles584 <154008927+yellowsparkles584@users.noreply.github.com>
2026-09-11 12:37:40 +1000

    Add files via upload

---
+++ include/xgameruntime.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) the Wine project
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+/* April 2026 Release of GDK */
+#define GDKC_VERSION 10002L
+#define GAMING_SERVICES_VERSION 7822L
+
+#include <xaccessibility.h>
+#include <xappcapture.h>
+#include <xasync.h>
+#include <xasyncprovider.h>
+#include <xdisplay.h>
+#include <xerror.h>
+#include <xgame.h>
+#include <xgameactivation.h>
+#include <xgameerr.h>
+#include <xgameevent.h>
+#include <xgameinvite.h>
+#include <xgameprotocol.h>
+#include <xgameruntimefeature.h>
+#include <xgameruntimetypes.h>
+#include <xgamesave.h>
+#include <xgamestreaming.h>
+#include <xgameui.h>
+#include <xnetworking.h>
+#include <xpackage.h>
+#include <xpersistentlocalstorage.h>
+#include <xspeechsynthesizer.h>
+#include <xstore.h>
+#include <xsystem.h>
+#include <xtaskqueue.h>
+#include <xuser.h>
